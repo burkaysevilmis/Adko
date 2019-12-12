3ae55939498625588af98c3ce17b55e4e09f1618
Burkay SEVİLMİŞ <burkay.sevilmis@gmail.com>
2019-12-12 11:23:49 +0300

    root file edit

--- src/Root.js
+++ src/Root.js
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
-import {Dimensions} from 'react-native';
+import { Dimensions } from 'react-native';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
@@ -8,7 +7,7 @@ import Icon from 'react-native-vector-icons/Ionicons';
 import homeScreen from './screen/Home';
 import HomeTest from './screen/HomeTest';
 import Menu from './components/menu';
-const {width,heigth} =Dimensions.get('window');
+const { width, heigth } = Dimensions.get('window');
 const HomeStack = createStackNavigator({
     Home: {
         screen: homeScreen,
@@ -23,13 +22,16 @@ const HomeStack = createStackNavigator({
         }),
 
     },
-    Test:{
-        screen:HomeTest
+    Login: {
+        screen: loginScreen,
+    },
+    Test: {
+        screen: HomeTest
     },
 }, {
     headerLayoutPreset: 'center',
-    initialRouteName:'Home'
- 
+    initialRouteName: 'Login'
+
 },
 
 );
@@ -38,13 +40,14 @@ const Drawer = createDrawerNavigator({
         screen: HomeStack,
         navigationOptions: {
             drawerLabel: 'Anasayfa',
-            drawerIcon: ({tintColor}) => (
+            drawerIcon: ({ tintColor }) => (
                 <Icon name="ios-home" size={22} color={tintColor} />
-              ),
+            ),
         },
     },
-    Test:{
-screen:HomeStack
+
+    Test: {
+        screen: HomeStack
     },
 },
     {
@@ -52,44 +55,10 @@ screen:HomeStack
             activeTintColor: 'white',
             activeBackgroundColor: '#404E67',
             inactiveBackgroundColor: '#375649',
-          },
+        },
         drawerBackgroundColor: '#404E67',
         drawerWidth: width / 2,
         drawerType: 'slide',
     }
 )
 export default createAppContainer(Drawer);
-=======
-import {createDrawerNavigator} from 'react-navigation-drawer';
-import {createStackNavigator} from 'react-navigation-stack';
-import {createAppContainer} from 'react-navigation';
-import homeScreen from './screen/Home';
-import loginScreen from './screen/Login';
-const HomeStack = createStackNavigator(
-  {
-    Home: {
-      screen: homeScreen,
-      navigationOptions: {
-        title: 'ADKO PORTAL',
-        headerTintColor: 'white',
-        headerStyle: {
-          backgroundColor: '#404E67',
-        },
-      },
-    },
-    Login: {
-      screen: loginScreen,
-    },
-  },
-  {initialRouteName: 'Login', headerLayoutPreset: 'center'},
-);
-const Drawer = createDrawerNavigator({
-  Home: {
-    screen: HomeStack,
-    navigationOptions: {
-      drawerLabel: 'Anasayfa',
-    },
-  },
-});
-export default createAppContainer(Drawer);
->>>>>>> b4923756ec696b967a0fb63f0d795fcf4a09625e
