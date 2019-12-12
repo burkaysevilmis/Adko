e41dec5d4f02debecef69923ef522f0e42f65276
Burkay SEVİLMİŞ <burkay.sevilmis@gmail.com>
2019-12-12 17:07:35 +0300

    education screen

---
+++ src/screen/Education.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { View, Text, ScrollView } from 'react-native';
+
+
+export default class Education extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+    render() {
+        const veri=[];
+        for (let index = 1; index < 10; index++) {
+            veri.push(
+                <View style={{  marginTop:20,width: '100%', height: 'auto', alignItems: 'center' }}>
+                <View style={{ width: '80%', height: 45, backgroundColor: '#2CC3D9',justifyContent:'center',alignItems:'center',borderRadius:15 }}>
+            <Text style={{color:'white',fontSize:15}}>{index}. INDOMIE ÜRÜN EĞİTİM</Text>
+                </View>
+            </View>
+            )
+        }
+        return (
+            <View style={{ flex: 1, alignItems: 'center' }}>
+                <View style={{marginTop:'10%',width: '90%',shadowOffset: { width: 10, height: 10 } }}>
+                    <View style={{justifyContent:'center',alignItems:'center'}}>
+                        <Text style={{ fontSize: 25 }}>EĞİTİMLER</Text>
+                    </View>
+                 
+                        <ScrollView>
+                          {veri}
+                        </ScrollView>
+                       
+                </View>
+            </View>
+        );
+    }
+}
