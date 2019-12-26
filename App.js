49be688c1ca3a1ae22b0eaabe987907ec5352ce2
osmancansiz <ocansiz@st.medipol.edu.tr>
2019-12-26 09:21:51 +0300

    new login page desing in root

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React, {Component} from 'react';
 import Root from './src/Root';
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
 
   render() {
-    return (
-     
-        <Root />
-      
-
-    );
+    return <Root />;
   }
 }
--- src/screen/Login.js
+++ src/screen/Login.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react';
-import { View, Text, Image, TextInput, Button, TouchableOpacity, Dimensions,SafeAreaView } from 'react-native';
-const { width, height } = Dimensions.get('window');
+/* eslint-disable react-native/no-inline-styles */
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  Dimensions,
+  SafeAreaView,
+} from 'react-native';
+const {width, height} = Dimensions.get('window');
 export default class LoginTest extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
   static navigationOptions = {
     headerShown: false,
@@ -14,23 +23,42 @@ export default class LoginTest extends Component {
   render() {
     const {navigate} = this.props.navigation;
     return (
-      <SafeAreaView style={{ height:height,width:width,backgroundColor:'#404E67'}} >
-        <View style={{height:height*.75,width:width,alignItems:'center'}}>
-<Image style={{marginTop:height/5}} source={require('../img/adko.png')}/>
+      <SafeAreaView
+        style={{height: height, width: width, backgroundColor: '#404E67'}}>
+        <View
+          style={{height: height * 0.75, width: width, alignItems: 'center'}}>
+          <Image
+            style={{marginTop: height / 5}}
+            source={require('../img/adko.png')}
+          />
         </View>
-        <View style={{height:height*.25,width:width,backgroundColor:'white'}}>
-
-        </View>
-        <View style={{ shadowColor: 'black',
+        <View
+          style={{
+            height: height * 0.25,
+            width: width,
+            backgroundColor: 'white',
+          }}
+        />
+        <View
+          style={{
+            shadowColor: 'black',
             shadowOpacity: 1.5,
             elevation: 8,
             shadowRadius: 20,
-            shadowOffset: {width: 1, height: 2},position:'absolute',height:height/3,bottom:62,width:width*0.9,alignSelf:'center',backgroundColor:'white',borderRadius:16}}>
-        <View style={{margin: 15}}>
+            shadowOffset: {width: 1, height: 2},
+            position: 'absolute',
+            height: height / 3,
+            bottom: 62,
+            width: width * 0.9,
+            alignSelf: 'center',
+            backgroundColor: 'white',
+            borderRadius: 16,
+          }}>
+          <View style={{margin: 15}}>
             <Text style={{color: 'gray'}}>Kullanıcı Adı</Text>
             <TextInput
               style={{
-                height: height*0.06,
+                height: height * 0.06,
                 borderBottomColor: 'gray',
                 borderBottomWidth: 1,
               }}
@@ -39,8 +67,9 @@ export default class LoginTest extends Component {
           <View style={{margin: 15}}>
             <Text style={{color: 'gray'}}>Şifre</Text>
             <TextInput
+              secureTextEntry={true}
               style={{
-                height: height*0.06,
+                height: height * 0.06,
                 borderBottomColor: 'gray',
                 borderBottomWidth: 1,
               }}
@@ -49,14 +78,16 @@ export default class LoginTest extends Component {
           <View
             style={{
               backgroundColor: '#404E67',
-              width: width*0.5,
+              width: width * 0.5,
               justifyContent: 'center',
               alignSelf: 'center',
               borderRadius: 17,
-              height: height*0.07,
-              marginTop:height*0.03,
+              height: height * 0.07,
+              marginTop: height * 0.015,
             }}>
-            <TouchableOpacity    onPress={() => navigate('Home')} style={{justifyContent: 'center'}}>
+            <TouchableOpacity
+              onPress={() => navigate('Home')}
+              style={{justifyContent: 'center'}}>
               <Text
                 style={{alignSelf: 'center', color: '#F8FAF9', fontSize: 16}}>
                 Giriş Yap
